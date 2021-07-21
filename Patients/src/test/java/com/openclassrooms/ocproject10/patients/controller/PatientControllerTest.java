b0512d49d09622b4b7162a86eebeba94311f1e6a
Musapa <ajagodic1990@gmail.com>
2021-07-21 18:01:48 +0200

    adding tests addPatient form and validate

--- Patients/src/test/java/com/openclassrooms/ocproject10/patients/controller/PatientControllerTest.java
+++ Patients/src/test/java/com/openclassrooms/ocproject10/patients/controller/PatientControllerTest.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.ocproject10.patients.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -52,25 +53,48 @@ public class PatientControllerTest {
 	public void createPatient() {
 		patient = new Patient();
 		Patient patient = new Patient();
-		patient.setGivenName("User_1");
-		patient.setFamilyName("Family_1");
+		patient.setGivenName("Test_1");
+		patient.setFamilyName("TestFamily_1");
 		patient.setDob(LocalDate.of(Integer.parseInt("1990"), Integer.parseInt("04"), Integer.parseInt("21")));
 		patient.setSex("Male");
-		patient.setAddress("Address_1");
-		patient.setPhone("033552528");
+		patient.setAddress("TestAddress_1");
+		patient.setPhone("033111111");
 
 		patient = patientRepository.save(patient);
 	}
-	
+
 	@Test
 	public void getAllPatients() throws Exception {
 		List<Patient> patientList = new ArrayList<>();
 		patientList.add(patient);
-		
+
 		MvcResult result = mockMvc.perform(get("/patient/list")).andExpect(view().name("patient/list"))
 				.andExpect(model().errorCount(0)).andExpect(status().isOk()).andReturn();
 
 		String content = result.getResponse().getContentAsString();
 		System.out.println("Content" + content);
 	}
+
+	@Test
+	public void addPatientForm() throws Exception {
+		MvcResult result = mockMvc.perform(get("/patient/add")).andExpect(view().name("patient/add"))
+				.andExpect(model().errorCount(0)).andExpect(status().isOk()).andReturn();
+
+		String content = result.getResponse().getContentAsString();
+		System.out.println("Content" + content);
+	}
+
+	@Test
+	public void addPatientValidate() throws Exception {
+		MvcResult result = mockMvc
+				.perform(post("/patient/validate").param("id", "10").param("givenName", "Test_2")
+						.param("familyName", "TestFamily_2").param("dob", "1990-04-21").param("sex", "Male")
+						.param("address", "TestAddress_2").param("phone", "033222222"))
+				.andExpect(view().name("redirect:/patient/list")).andExpect(status().is3xxRedirection())
+				.andExpect(model().errorCount(0)).andReturn();
+
+		String content = result.getResponse().getContentAsString();
+		System.out.println("Content" + content);
+	}
+
 }
